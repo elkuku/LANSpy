78085c8925361817ae152985465d73457b1320c9
elkuku <der.el.kuku@gmail.com>
2017-06-20 13:08:24 -0500

    Fix a path

--- bin/pingtest.sh
+++ bin/pingtest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ROOT=`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
+ROOT=`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."`
 
 test1=$(ping -c 4 google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
 test2=$(ping -c 4 github.com | tail -1| awk '{print $4}' | cut -d '/' -f 2)
