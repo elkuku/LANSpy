bfbcd7f1c53bebd128895d41d149778d55445470
elkuku <der.el.kuku@gmail.com>
2017-06-22 01:03:48 -0500

    Start Maptest

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,12 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            ]
+        );
     }
 
     /**
@@ -25,12 +28,24 @@ class DefaultController extends Controller
      */
     public function pingtestAction(PingTest $pingtest)
     {
-        $pingtest->hello();
-        // replace this example code with whatever you need
-        return $this->render('tests/pingtest.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'hello' => $pingtest->hello(),
-            'tests' => $pingtest->readTests(), 'actDate' => (new \DateTime())->format('Ymd')
-        ]);
+        return $this->render(
+            'tests/pingtest.html.twig',
+            [
+                'tests'   => $pingtest->readTests(),
+                'actDate' => (new \DateTime())->format('Ymd'),
+            ]
+        );
+    }
+
+    /**
+     * @Route("maptest", name="maptest")
+     */
+    public function maptestAction()
+    {
+        return $this->render(
+            'tests/maptest.html.twig',
+            [
+            ]
+        );
     }
 }
