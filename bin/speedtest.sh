ded9dd928e937e96f5da69e1e16cae8eb4adac1d
elkuku <der.el.kuku@gmail.com>
2017-07-08 08:39:15 -0500

    Fix speedtest path

--- bin/speedtest.sh
+++ bin/speedtest.sh
@@ -2,4 +2,4 @@
 
 ROOT=`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."`
 
-${ROOT}/lib/speedtest/speedtest.py --no-pre-allocate --json >> ${ROOT}/results/speedtest-$(date +"%Y-%m-%d").txt
+${ROOT}/bin/speedtest/speedtest.py --no-pre-allocate --json >> ${ROOT}/results/speedtest-$(date +"%Y-%m-%d").txt
